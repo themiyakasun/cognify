51586dce6753bfe8243ec88661e485e3ec6399ce
Themiya Kasun <themiyakasun.tb@gmail.com>
2025-09-03 15:47:54 +0530

    Common Repositories (Repository Pattern)

---
+++ backend/CognifyAPI/Interfaces/IApplicationRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace CognifyAPI.Interfaces
+{
+    public interface IApplicationRepository<T>
+    {
+        Task<T> CreateAsync(T dbRecord);
+        Task<T> UpdateAsync(T dbRecord);
+        Task<List<T>> GetAllAync();
+        Task<T?> GetAsync(Expression<Func<T, bool>> filter);
+        Task<T> DeleteAsync(T dbRecord);
+    }
+}
---
+++ backend/CognifyAPI/Repositories/ApplicationRepository.cs
@@ -0,0 +1,54 @@
+﻿using CognifyAPI.Data;
+using CognifyAPI.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CognifyAPI.Repositories
+{
+    public class ApplicationRepository<T> : IApplicationRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public ApplicationRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public async Task<T> CreateAsync(T dbRecord)
+        {
+            await _dbSet.AddAsync(dbRecord);
+            await _dbContext.SaveChangesAsync();
+            return dbRecord;
+        }
+
+        public async Task<T> DeleteAsync(T dbRecord)
+        {
+            _dbSet.Remove(dbRecord);
+            await _dbContext.SaveChangesAsync();
+            return dbRecord;
+        }
+
+        public async Task<List<T>> GetAllAync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetAsync(Expression<Func<T, bool>> filter)
+        {
+            var values = await _dbSet.Where(filter).FirstOrDefaultAsync();
+
+            if (values != null) return values;
+
+            return null;
+        }
+
+        public async Task<T> UpdateAsync(T dbRecord)
+        {
+            _dbContext.Update(dbRecord);
+            await _dbContext.SaveChangesAsync();
+            return dbRecord;
+        }
+    }
+}
